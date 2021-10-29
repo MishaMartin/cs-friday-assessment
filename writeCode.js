3134d9cfdaa8ec2510d42221a1170d9734875a36
Misha Martin <mishanicolemartin@gmail.com>
2021-10-29 12:03:10 -0600

    Added coding challenge document and successfully completed one

---
+++ writeCode.js
@@ -0,0 +1,41 @@
+//Sum Zero
+//Write a function that takes in an array of numbers. The function should return True if any two numberss in list sum to 0, and false otherwise.
+
+//Code Here
+
+//Unique Characters
+//Write a function that takes in a single word, as a string. It should return True if that word contains only unique characters. Return False otherwise.
+
+//Code Here
+
+//Panagram Sentence
+//A pangram is a sentence that contains all the letters of the English alphabet at least once, like “The quick brown fox jumps over the lazy dog.”
+//Write a function to check a sentence to see if it is a pangram or not.
+
+//Code Here
+
+//Longest Word
+//Write a function, find_longest_word, that takes a list of words and returns the length of the longest one.
+
+function longestWord(string) {
+    var str = string.split(" ");
+    var longest = 0;
+    var word = null;
+
+    str.forEach(function(str) {
+        if (longest < str.length) {
+            longest = str.length;
+            console.log(longest)
+            word = str;
+            console.log(word)
+        }
+    });
+
+return word;
+}
+
+sentence = ("I enjoy eating pasta")
+
+let answer = longestWord(sentence)
+
+console.log(answer)
