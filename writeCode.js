ed953ed68dd6d5231ae34144515e353f721b5d6b
Misha Martin <mishanicolemartin@gmail.com>
2021-10-29 14:52:04 -0600

    Additional changes to the first solution

--- writeCode.js
+++ writeCode.js
@@ -1,7 +1,21 @@
 //Sum Zero
-//Write a function that takes in an array of numbers. The function should return True if any two numberss in list sum to 0, and false otherwise.
+//Write a function that takes in an array of numbers. The function should return True if any two numbers in list sum to 0, and false otherwise.
 
 //Code Here
+function trueZero(num){
+    let sum
+    for(let i=0; i<num.length; i++){
+        let num1 = num[i]
+        let num2 = num[i]
+        console.log(num1)
+        console.log(num2)
+    }
+}
+
+let numArr = [4, 5, 10, 6]
+let numArr2 = [2, 0 , -2 , 8]
+
+trueZero(numArr)
 
 //Unique Characters
 //Write a function that takes in a single word, as a string. It should return True if that word contains only unique characters. Return False otherwise.
@@ -17,25 +31,25 @@
 //Longest Word
 //Write a function, find_longest_word, that takes a list of words and returns the length of the longest one.
 
-function longestWord(string) {
-    var str = string.split(" ");
-    var longest = 0;
-    var word = null;
+// function longestWord(string) {
+//     var str = string.split(" ");
+//     var longest = 0;
+//     var word = null;
 
-    str.forEach(function(str) {
-        if (longest < str.length) {
-            longest = str.length;
-            console.log(longest)
-            word = str;
-            console.log(word)
-        }
-    });
+//     str.forEach(function(str) {
+//         if (longest < str.length) {
+//             longest = str.length;
+//             console.log(longest)
+//             word = str;
+//             console.log(word)
+//         }
+//     });
 
-return word;
-}
+// return word;
+// }
 
-sentence = ("I enjoy eating pasta")
+// sentence = ("I enjoy eating pasta")
 
-let answer = longestWord(sentence)
+// let answer = longestWord(sentence)
 
-console.log(answer)
+// console.log(answer)
